d858b60d46cec04787f175d9a6c0471ecca33778
j7 <qnljoy@gmail.com>
2021-09-14 09:27:50 +0200

    First commit

---
+++ main.rb
@@ -0,0 +1,12 @@
+require 'xcodeproj'
+
+project_path = ARGV[0]
+project = Xcodeproj::Project.open(project_path)
+
+project.targets.each do |target|
+  target.build_configurations.each do |config|
+    config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = target.name == 'Unity-iPhone' ? 'YES' : 'NO'
+  end
+end
+
+project.save(nil)
